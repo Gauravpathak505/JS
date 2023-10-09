280b0db81f4137ac160b0b43822824b2e372d6a6
Gaurav Pathak <107796471+Gauravpathak505@users.noreply.github.com>
2023-10-09 15:35:14 +0000

    conversion of datatype

---
+++ 1_basics/03_conversion.js
@@ -0,0 +1,16 @@
+let score=33;
+console.log(typeof score);
+let score1="33abc";
+console.log(typeof score1);
+let valueInNumber=Number(score1);
+console.log(typeof valueInNumber);
+console.log(valueInNumber) // output: NAN (not a number)
+
+let value='';
+let booleanLoggedIn=Boolean(value);
+console.log(booleanLoggedIn);  // ouput : true 
+// 0: false
+//1: true 
+//"": false
+// "gaurav" : true 
+
