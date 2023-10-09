97549f996e50719be83126e7a615aca668be74c3
Gaurav Pathak <107796471+Gauravpathak505@users.noreply.github.com>
2023-10-09 15:36:05 +0000

    Variables of js

---
+++ 1_basics/1_variables.js
@@ -0,0 +1,7 @@
+const accountId=12345;
+let accountEmail='guaravpathak@gmail.com';
+var accountPassword='12345';
+accountCity='patna';
+
+// accountId=2345;
+console.log([accountId ,accountEmail ,accountPassword ,accountCity]);
