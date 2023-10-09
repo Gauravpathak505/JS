6783df3a32a809a7ba6f706eecd1979a25412e11
Gaurav Pathak <107796471+Gauravpathak505@users.noreply.github.com>
2023-10-09 15:35:35 +0000

    datatypes

---
+++ 1_basics/2_datatypes.js
@@ -0,0 +1,10 @@
+let name="gaurav";
+let age=18;
+let isloggedIn=false;
+
+console.log([name , age, isloggedIn]);
+
+// null => standalone value , this is object you can defined also
+
+console.log(typeof null); // output : object
+console.log(typeof undefined); // output: undefined 
