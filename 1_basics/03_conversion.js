38e2e0d38d8e32940cb64f6eda96b089a71d6e7b
Gaurav Pathak <107796471+Gauravpathak505@users.noreply.github.com>
2023-10-09 16:00:05 +0000

    #

--- 1_basics/03_conversion.js
+++ 1_basics/03_conversion.js
@@ -1,16 +1,42 @@
-let score=33;
-console.log(typeof score);
-let score1="33abc";
-console.log(typeof score1);
-let valueInNumber=Number(score1);
-console.log(typeof valueInNumber);
-console.log(valueInNumber) // output: NAN (not a number)
+// let score=33;
+// console.log(typeof score);
+// let score1="33abc";
+// console.log(typeof score1);
+// let valueInNumber=Number(score1);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber) // output: NAN (not a number)
 
-let value='';
-let booleanLoggedIn=Boolean(value);
-console.log(booleanLoggedIn);  // ouput : true 
+// let value='';
+// let booleanLoggedIn=Boolean(value);
+// console.log(booleanLoggedIn);  // ouput : true 
 // 0: false
 //1: true 
 //"": false
 // "gaurav" : true 
+// let someNumber=33;
+// let stringNumber=String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+// this is conversion a number into String 
 
+// **************************  Operations   ***************************
+
+let value=3;
+let negvalue=-value;
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2/3);
+// console.log(2%3);
+// console.log(2**6);
+
+let str='hello'
+let str1=' gaurav'
+let str3=str+str1;
+console.log(str3);
+
+console.log(1+"2"+3); // in this case js understand the first input datatypes like first is integer 
+console.log("2"+2+2);// here is string so all value will be concate
+console.log(1+2+"44");// here first two vlue will add after string value will be conacte 
