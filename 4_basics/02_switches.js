a172607257179b9dc8fcb0a5dcffbe670961ac69
Gaurav Pathak <107796471+Gauravpathak505@users.noreply.github.com>
2023-10-19 03:26:07 +0000

    Switch

---
+++ 4_basics/02_switches.js
@@ -0,0 +1,42 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+let month=7
+switch (month) {
+    case 1:
+        console.log('january')
+        
+        
+        break;
+    case 2:
+        console.log('feb')
+        
+        
+        break;
+    case 3:
+        console.log('march')
+        
+        
+        break;
+    case 4:
+        console.log('april')
+        
+        
+        break;
+    case 5:
+        console.log('may')
+        
+        
+        break;
+
+    default:
+        console.log("please select the 1 to 12 number");
+       
+}
